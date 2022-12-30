b4f6dcd0e213eb0165d2ce7da01e967050c1f3cd
Krishna8074123 <112103261+Krishna8074123@users.noreply.github.com>
2022-12-30 13:39:29 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my_first_instance"{
   ami                    = "ami-02045ebddb047018b"
   instance_type          = "t2.micro"
   key_name               = "singporekey"
-  subnet_id = aws_subnet.public_subnet[count.index].id
+  subnet_id = aws_subnet.public_subnet[0].id
   security_groups         = [aws_security_group.aws_sg.id]
   tags = {
  Name = "ec2_instance"
